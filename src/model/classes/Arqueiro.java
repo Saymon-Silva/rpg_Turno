2b3079ccb459fc8b98a33ee10f6673728e00e8f7
saymon_p_silva <saymonsilv401@gmail.com>
2023-07-18 19:20:35 -0300

    salva cocotinha

---
+++ src/model/classes/Arqueiro.java
@@ -0,0 +1,10 @@
+package model.classes;
+
+import model.Classe;
+
+public class Arqueiro extends Classe {
+
+    public Arqueiro(int maxVida, int dano) {
+        super(maxVida, dano);
+    }
+}
---
+++ src/model/classes/Mago.java
@@ -0,0 +1,10 @@
+package model.classes;
+
+import model.Classe;
+
+public class Mago extends Classe {
+
+    public Mago(int maxVida, int dano) {
+        super(maxVida, dano);
+    }
+}
